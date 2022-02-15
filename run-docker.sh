29cfd40a1308f3956bcc566dd5a4dfda2fc573ba
minsik.yoon <yms9654@gmail.com>
2022-02-15 16:30:27 +0900

    update

--- run-docker.sh
+++ run-docker.sh
@@ -1,1 +1,11 @@
-docker run -v ~/workspace/pororo/data:/app/data -it -p 8888:8888 pororo:0.1 /bin/bash
+if [ -z "$1" ]
+    then
+        echo "Please port number."
+        exit 1
+fi
+
+docker run --mount type=bind,source="$(pwd)"/scripts,target=/app/scripts -it -p $1:8888 yms9654/stt-demo /bin/bash
+# docker run --mount type=bind,source="$(pwd)"/app,target=/app -it -p $1:8888 pororo:0.1 /bin/bash
+# docker run -it -p $1:8888 yms9654/stt-demo /bin/bash
+
+
